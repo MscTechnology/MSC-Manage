2934643a52a0d8fe1a41e11c9aab128a4900b0bd
Berkayergun16 <berkay.ergun16@gmail.com>
2022-03-22 15:07:36 +0300

    personel login added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import Dashboard from "./pages/Dashboard/Dashboard";
 import "./styles.css";
 import AdminLogin from "./pages/Login/Admin/AdminLogin";
+import PersonelLogin from "./pages/Login/Personel/PersonelLogin";
 function App() {
   return (
     <div className="App">
         
-      <AdminLogin />
+      <PersonelLogin/>
     </div>
   );
 }
