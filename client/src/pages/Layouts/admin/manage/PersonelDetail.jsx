0ce25109490bafe38949faf4f169e11cedc61d0e
Berkayergun16 <berkay.ergun16@gmail.com>
2022-03-28 10:59:23 +0300

    personel detay

--- client/src/pages/Layouts/admin/manage/PersonelDetail.jsx
+++ client/src/pages/Layouts/admin/manage/PersonelDetail.jsx
@@ -2,28 +2,43 @@ import "../../../../styles.css";
 import { gql, useQuery } from "@apollo/client";
 import { Button } from "@mui/material";
 import { NavLink,useParams } from "react-router-dom";
+import { useGetUserDetailQuery } from "generated/graphql";
+import { useEffect, useState } from "react";
 
 
-const GET_DETAIL = gql`
-  query GetUserDetail {
-    users(id:4) {
-      name
-      surname
-      username
-    } 
-  }
-`;
+
+
 
 function PersonelDetail() {
-  const { loading, error, data } = useQuery(GET_DETAIL);
-  console.log(data);
-  console.log(data?.users?.name);
+  const [rowData, setRowData] = useState({});
   const {id} = useParams()
   console.log(id);
+
+  const { data, loading, error } = useGetUserDetailQuery({
+    variables: {
+       prmId: parseInt(id)// value for 'prmId'
+    },
+  });
+ 
+  useEffect(() => {
+    if (data) {
+      if (data?.usersById?.length > 0) {
+        setRowData(data?.usersById[0]);
+      }
+      
+    }
+    
+  } ,[data])
+
+  console.log(rowData);
+ 
   return (
     <div className="adminPage">
     <div className="admin-title">
       {`${id} id' li kullanıcının bilgileri`}
+      {
+      rowData.name
+      }
     </div>
     {id}
     
