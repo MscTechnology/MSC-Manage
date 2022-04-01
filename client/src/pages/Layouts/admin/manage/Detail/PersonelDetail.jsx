5821e7b843615d671d72a7984dbd5f59bf2903d5
Kubra <kubrasambur123@gmail.com>
2022-04-01 18:00:29 +0300

    01.04.22 last commit

--- client/src/pages/Layouts/admin/manage/Detail/PersonelDetail.jsx
+++ client/src/pages/Layouts/admin/manage/Detail/PersonelDetail.jsx
@@ -1,5 +1,5 @@
 import "../../../../../styles.css";
-import { Button, Typography, CardMedia, CardContent, CardActions, Card, Switch } from "@mui/material";
+import { Button, Typography, CardMedia, CardContent, CardActions, Card, Switch,FormControlLabel  } from "@mui/material";
 import { NavLink, useParams } from "react-router-dom";
 import { useGetUserDetailQuery, useGetUserQuery, useUpdateUserMutation } from "generated/graphql";
 import { useEffect, useState } from "react";
@@ -7,6 +7,9 @@ import Loading from "../../../../../components/Loading/Loading";
 import Error from "../../../../../components/Error/Error";
 import { useSelector, useDispatch } from "react-redux";
 import { setActive } from "../../../../../../src/store/User/UserSlice"
+import { styled } from '@mui/material/styles';
+import SwitchUnstyled, { switchUnstyledClasses } from '@mui/base/SwitchUnstyled';
+
 const label = { inputProps: { 'aria-label': 'Switch demo' } }
 
 function PersonelDetail() {
@@ -68,11 +71,84 @@ function PersonelDetail() {
     })
     setChecked(e.target.checked);
     dispatch(setActive(!checked));
-    }
-    
-    
+    } 
   }
-
+  const blue = {
+    500: '#007FFF',
+  };
+  
+  const grey = {
+    400: '#BFC7CF',
+    500: '#AAB4BE',
+    600: '#6F7E8C',
+  };
+  const Root = styled('span')(
+    ({ theme }) => `
+    font-size: 0;
+    position: relative;
+    display: inline-block;
+    width: 40px;
+    height: 20px;
+    margin: 10px;
+    cursor: pointer;
+  
+    &.${switchUnstyledClasses.disabled} {
+      opacity: 0.4;
+      cursor: not-allowed;
+    }
+  
+    & .${switchUnstyledClasses.track} {
+      background: ${theme.palette.mode === 'dark' ? grey[600] : grey[400]};
+      border-radius: 10px;
+      display: block;
+      height: 100%;
+      width: 100%;
+      position: absolute;
+    }
+  
+    & .${switchUnstyledClasses.thumb} {
+      display: block;
+      width: 14px;
+      height: 14px;
+      top: 3px;
+      left: 3px;
+      border-radius: 16px;
+      background-color: #fff;
+      position: relative;
+      transition: all 200ms ease;
+    }
+  
+    &.${switchUnstyledClasses.focusVisible} .${switchUnstyledClasses.thumb} {
+      background-color: ${grey[500]};
+      box-shadow: 0 0 1px 8px rgba(0, 0, 0, 0.25);
+    }
+  
+    &.${switchUnstyledClasses.checked} {
+      .${switchUnstyledClasses.thumb} {
+        left: 22px;
+        top: 3px;
+        background-color: #fff;
+      }
+  
+      .${switchUnstyledClasses.track} {
+        background: ${blue[500]};
+      }
+    }
+  
+    & .${switchUnstyledClasses.input} {
+      cursor: inherit;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      opacity: 0;
+      z-index: 1;
+      margin: 0;
+    }
+    `,
+  );
+  
 
   return (
     <div className="detailPage">
@@ -80,6 +156,8 @@ function PersonelDetail() {
         Informations
 
       </div>
+      
+      
       <div className="name">
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia className="cardMedia"
@@ -111,7 +189,7 @@ function PersonelDetail() {
             </div>
             <div className="active-deactive">
               <div>deactive</div>
-              <Switch {...label} defaultChecked onChange={handleChange} checked={checked}/>
+              <SwitchUnstyled component={Root} onChange={handleChange} checked={checked}/>
               <div>Active</div>
             </div>
           </CardActions>
