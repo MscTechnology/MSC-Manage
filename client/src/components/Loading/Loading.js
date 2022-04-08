7be5ffe7012b652ea10c70d0213da0c569dff63a
Kubra <kubrasambur123@gmail.com>
2022-04-08 10:13:31 +0300

    main

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import Lottie from 'lottie-react';
-import LoadingAnimation from "../../assets/animations/loading.json"
+import React from "react";
+import Lottie from "lottie-react";
+import LoadingAnimation from "../../assets/animations/loading.json";
 
 function Loading() {
+  
   return (
     <Lottie
-    animationData={LoadingAnimation} loop={true} autoPlay={true}
+      animationData={LoadingAnimation}
+      loop={true}
+      autoPlay={true}
+      style={{ width: "30%", marginLeft: "35%", marginTop: "8%" }}
     />
-  )
+  );
 }
 
-export default Loading
+export default Loading;
