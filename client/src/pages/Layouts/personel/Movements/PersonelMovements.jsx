fed740c59fab847c1a6733fbf2b79cfc6a5771c0
Kubra <kubrasambur123@gmail.com>
2022-04-12 14:03:40 +0300

    personel movement sayfası güncellendi

--- client/src/pages/Layouts/personel/Movements/PersonelMovements.jsx
+++ client/src/pages/Layouts/personel/Movements/PersonelMovements.jsx
@@ -17,7 +17,7 @@ const PersonelMovements = () => {
     const { id } = useParams()
     const user = useSelector((state) => state.users.user);
 
-    const { data, loading, error } = useGetUserMovementByIdQuery({
+    const { data, loading, error,refetch } = useGetUserMovementByIdQuery({
         variables: {
             prmId: parseInt(id)
         },
@@ -56,35 +56,13 @@ const PersonelMovements = () => {
                     createuser: user.createuser,
                 }
             }
+        }).then((res) => {
+            refetch()
         })
 
     };
 
     const columns = [
-        {
-            field: "usersid",
-            headerName: "User id",
-            width: 130,
-            valueFormatter: (params) => {
-                return params.row?.users?.userid;
-            },
-        },
-        {
-            field: "users.name",
-            headerName: "Name",
-            width: 130,
-            valueFormatter: (params) => {
-                return params.api.state.rows.idRowsLookup[params.id].users.name;
-            },
-        },
-        {
-            field: "users.surname",
-            headerName: "Surname",
-            width: 130,
-            valueFormatter: (params) => {
-                return params.api.state.rows.idRowsLookup[params.id].users.surname;
-            },
-        },
         {
             field: "transactiondate",
             headerName: "Transaction Date",
@@ -133,7 +111,6 @@ const PersonelMovements = () => {
                     columns={columns}
                     pageSize={5}
                     rowsPerPageOptions={[5, 10, 20, 30, 40, 50]}
-                    checkboxSelection
                     autoPageSize
                     pagination
                 />
